224e9a855b681dc7280acc92463e0e80c5070af5
wolfstons <paczi.balazs@gmail.com>
2026-04-14 12:41:05 +0200

    gombok kész

--- js/Kepek.js
+++ js/Kepek.js
@@ -1,14 +1,19 @@
 import Kep from "./Kep.js";
 export default class Kepek {
   #lista;
+  #currentIndex;
 
   constructor(lista, szuloElem,fokep) {
     this.#lista = lista;
     this.szuloElem = szuloElem;
     this.fokep=fokep
+    this.elozoGomb = document.querySelector(".elozo");
+    this.kovetkezoGomb = document.querySelector(".kovetkezo");
     this.megjelenit()
     this.esemeny()
     this.nagykep(0)
+    this.elozoGomb.addEventListener("click", () => this.elozo());
+    this.kovetkezoGomb.addEventListener("click", () => this.kovetkezo());
   }
   megjelenit() {
     this.szuloElem.innerHTML = "";
@@ -26,6 +31,7 @@ export default class Kepek {
   }
 
   nagykep(index){
+        this.#currentIndex=index
         const adat=this.#lista[index]
         let kod=`<div class="hattalmaskep">
         <img src="${adat.src}" alt="${adat.cim}">
@@ -35,4 +41,16 @@ export default class Kepek {
 
         this.fokep.innerHTML=kod
     }
+
+    kovetkezo() {
+    let uj = this.#currentIndex + 1;
+    if (uj >= this.#lista.length) uj = 0;
+    this.nagykep(uj);
+  }
+
+  elozo() {
+    let uj = this.#currentIndex - 1;
+    if (uj < 0) uj = this.#lista.length - 1;
+    this.nagykep(uj);
+  }
 }
